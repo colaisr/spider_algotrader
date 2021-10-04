ba8056a4e0c1053a772cea22cb5de4592404cdc6
colakamornik <cola.isr@gmail.com>
2021-10-04 13:53:31 +0300

    fgi collected in the beginning of spider

--- app/research/cnn_fgi_research.py
+++ app/research/cnn_fgi_research.py
@@ -5,19 +5,23 @@ import requests
 
 def get_cnn_fgi_rate():
     print('Getting an FGI index for today:')
-    url = "https://fear-and-greed-index.p.rapidapi.com/v1/fgi"
-    rk = os.environ.get('RAPIDAPI_KEY')
-    headers = {
-        'x-rapidapi-host': "fear-and-greed-index.p.rapidapi.com",
-        'x-rapidapi-key': rk
-        }
+    try:
+        url = "https://fear-and-greed-index.p.rapidapi.com/v1/fgi"
+        rk = os.environ.get('RAPIDAPI_KEY')
+        headers = {
+            'x-rapidapi-host': "fear-and-greed-index.p.rapidapi.com",
+            'x-rapidapi-key': rk
+            }
 
-    response = requests.request("GET", url, headers=headers)
-    rj=json.loads(response.text)
-    val=rj['fgi']['now']['value']
-    valtext=rj['fgi']['now']['valueText']
-    print(response.text)
-    return val,valtext
+        response = requests.request("GET", url, headers=headers)
+        rj=json.loads(response.text)
+        val=rj['fgi']['now']['value']
+        valtext=rj['fgi']['now']['valueText']
+        print(response.text)
+        return val,valtext
+    except Exception as e:
+        print('problem with getting: ', e)
+        return None,None
 
 if __name__ == '__main__':
     get_cnn_fgi_rate()
