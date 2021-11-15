0f93db74a6b9feb3c55de9e85a10005d350ad24d
colakamornik <cola.isr@gmail.com>
2021-11-16 00:03:17 +0300

    short price version added

--- app/data_hub/views.py
+++ app/data_hub/views.py
@@ -38,4 +38,11 @@ def current_stock_price_full(tickers):
     result=current_stock_price_full_w(tickers)
     return result
 
+# http://localhost:8000/data_hub/current_stock_price_short/AAPL,MSFT
+@data_hub.route('current_stock_price_short/<tickers>', methods=['GET'])
+@csrf.exempt
+def current_stock_price_short(tickers):
+    result=current_stock_price_short_w(tickers)
+    return result
+
 
