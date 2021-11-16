f64998c7c9125584de67bff71bc7e9dcfc0c4ac0
LilianaIL <liliana.isr@gmail.com>
2021-11-16 21:49:43 +0300

    cleane code

--- app/data_hub/views.py
+++ app/data_hub/views.py
@@ -6,7 +6,6 @@ from flask import (
 
 from app import csrf
 
-
 from flask_cors import cross_origin
 
 from app.research.fmp_wrapper import *
@@ -14,12 +13,11 @@ from app.research.fmp_wrapper import *
 data_hub = Blueprint('data_hub', __name__)
 
 
-# http://localhost:8000/data_hub/historical_daily_price_full?ticker=msft
-@data_hub.route('historical_daily_price_full', methods=['GET'])
+# http://localhost:8000/data_hub/historical_daily_price_full/msft
+@data_hub.route('historical_daily_price_full/<ticker>', methods=['GET'])
 @csrf.exempt
-def historical_daily_price_full():
-    ticker = request.args.get('ticker')
-    result=historical_daily_price_full_w(ticker)
+def historical_daily_price_full(ticker):
+    result = historical_daily_price_full_w(ticker)
     return jsonify(result)
 
 
@@ -27,7 +25,7 @@ def historical_daily_price_full():
 @data_hub.route('current_market_operation/', methods=['GET'])
 @csrf.exempt
 def current_market_operation():
-    result=current_market_operation_w()
+    result = current_market_operation_w()
     return jsonify(result)
 
 
@@ -35,15 +33,17 @@ def current_market_operation():
 @data_hub.route('current_stock_price_full/<tickers>', methods=['GET'])
 @csrf.exempt
 def current_stock_price_full(tickers):
-    result=current_stock_price_full_w(tickers)
+    result = current_stock_price_full_w(tickers)
     return jsonify(result)
+
 
 # http://localhost:8000/data_hub/current_stock_price_short/AAPL,MSFT
 @data_hub.route('current_stock_price_short/<tickers>', methods=['GET'])
 @csrf.exempt
 def current_stock_price_short(tickers):
-    result=current_stock_price_short_w(tickers)
+    result = current_stock_price_short_w(tickers)
     return jsonify(result)
+
 
 # http://localhost:8000/data_hub/stock_news?tickers=AAPL,FB,GOOG,AMZN&limit=50
 @data_hub.route('stock_news', methods=['GET'])
@@ -51,13 +51,13 @@ def current_stock_price_short(tickers):
 def stock_news():
     tickers = request.args.get('tickers')
     limit = request.args.get('limit')
-    result=stock_news_w(tickers,limit)
+    result = stock_news_w(tickers, limit)
     return jsonify(result)
+
 
 # http://localhost:8000/data_hub/average_sector_pe_today/Energy
 @data_hub.route('average_sector_pe_today/<sector>', methods=['GET'])
 @csrf.exempt
 def average_pe(sector):
-    result=average_sector_pe_today(sector)
+    result = average_sector_pe_today(sector)
     return jsonify(result)
-
